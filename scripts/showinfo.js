c1a6eec939310237f90b7ef7b2e8ac1029d0f4c3
FranciscoMunozP <francisco.munozpe@usm.cl>
2025-05-26 22:02:22 -0400

    mobile fix again

--- scripts/showinfo.js
+++ scripts/showinfo.js
@@ -38,11 +38,11 @@ function resolution(isMobile){
   modal4.style.height = `${Math.floor((modal_content.style.height)*0.22)}px`;
 
   if(isMobile){
-    modal_content.style.width = '90vh';
-    modal1.style.width = '32vh';
-    modal2.style.width = '32vh';
-    modal3.style.width = '58vh';
-    modal4.style.width = '58vh';
+    modal_content.style.width = '90vw';
+    modal1.style.width = '32vw';
+    modal2.style.width = '32vw';
+    modal3.style.width = '58vw';
+    modal4.style.width = '58vw';
   } else {
     modal_content.style.width = `${Math.ceil(screenWidth*0.6)}px`;
     modal1.style.width = `${Math.ceil((modal_content.style.width - 100)*0.35)}px`;
@@ -227,7 +227,7 @@ function show_sell() {
 
 function verificarOrientacion() {
   const isMobile = /Mobi|Android/i.test(navigator.userAgent);
-  const isPortrait = window.innerHeight > window.innerWidth;
+  const isPortrait = window.Height > window.Width;
   screenWidth = screen.width;
   screenHeight = screen.height;
   resolution((isMobile && isPortrait));
