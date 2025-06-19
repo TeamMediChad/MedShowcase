d74131be7a49cd6c261c9ea354b68b99e81965d0
FranciscoMunozP <francisco.munozpe@usm.cl>
2025-06-19 15:31:20 -0400

    Migración de DB

--- scripts/loadshowcase.js
+++ scripts/loadshowcase.js
@@ -1,209 +1,213 @@
-const member_container = document.getElementById('member-container');
-member_container.innerHTML = '';
+function renderMembers() {
+  const topData = getTop(capturedBy);
+  const member_container = document.getElementById('member-container');
+  member_container.innerHTML = '';
 
-// Obtener claves de miembros
-const miembros = Object.keys(memberData);
+  // Obtener claves de miembros
+  const miembros = Object.keys(memberData);
 
-// Para cada miembro
-miembros.forEach(key => {
-  const miembro = memberData[key];
-  if (!miembro) return;
+  // Para cada miembro
+  miembros.forEach(key => {
+    const miembro = memberData[key];
+    if (!miembro) return;
 
-  //verificar si tiene golden box
-  const card = document.createElement('div');
-  if(miembro.medallas.includes('b23')){
-    card.className= 'card-gold';
-  } else {
-    card.className= 'card';
-  }
-  
-  //contenedor de medallas en tarjeta
-  const iconContainer = document.createElement('div');
-  iconContainer.className = 'card-icon-container';
-  card.appendChild(iconContainer);
-
-  //MegaChad
-  if(miembro.rol == 'MegaChad'){
-
-    const icon = document.createElement('img');
-    icon.src = '../img/mega_icon.png';
-    icon.className = 'card-icon';
-
-    const tooltipWrapper = document.createElement('span');
-    tooltipWrapper.className = 'tooltip';
-
-    const tooltip = document.createElement('span');
-    tooltip.className = 'tooltiptext';
-    tooltip.textContent = 'MegaChad';
-
-    tooltipWrapper.appendChild(icon);
-    tooltipWrapper.appendChild(tooltip);
-
-    iconContainer.appendChild(tooltipWrapper);
-
+    //verificar si tiene golden box
+    const card = document.createElement('div');
+    if(miembro.medallas.includes('b23')){
+      card.className= 'card-gold';
+    } else {
+      card.className= 'card';
+    }
     
-  }
+    //contenedor de medallas en tarjeta
+    const iconContainer = document.createElement('div');
+    iconContainer.className = 'card-icon-container';
+    card.appendChild(iconContainer);
 
-  //GigaChad
-  if(miembro.rol == 'GigaChad'){
-    const icon = document.createElement('img');
-    icon.src = '../img/giga_icon.png';
-    icon.className = 'card-icon';
-    
-    const tooltipWrapper = document.createElement('span');
-    tooltipWrapper.className = 'tooltip';
+    //MegaChad
+    if(miembro.rol == 'MegaChad'){
 
-    const tooltip = document.createElement('span');
-    tooltip.className = 'tooltiptext';
-    tooltip.textContent = 'GigaChad';
+      const icon = document.createElement('img');
+      icon.src = '../img/mega_icon.png';
+      icon.className = 'card-icon';
 
-    tooltipWrapper.appendChild(icon);
-    tooltipWrapper.appendChild(tooltip);
+      const tooltipWrapper = document.createElement('span');
+      tooltipWrapper.className = 'tooltip';
 
-    iconContainer.appendChild(tooltipWrapper);
-  }
+      const tooltip = document.createElement('span');
+      tooltip.className = 'tooltiptext';
+      tooltip.textContent = 'MegaChad';
 
-  //MaxiChad
-  if(miembro.rol == 'MaxiChad'){
-    const icon = document.createElement('img');
-    icon.src = '../img/maxi_icon.png';
-    icon.className = 'card-icon';
-    
-    const tooltipWrapper = document.createElement('span');
-    tooltipWrapper.className = 'tooltip';
+      tooltipWrapper.appendChild(icon);
+      tooltipWrapper.appendChild(tooltip);
 
-    const tooltip = document.createElement('span');
-    tooltip.className = 'tooltiptext';
-    tooltip.textContent = 'MaxiChad';
+      iconContainer.appendChild(tooltipWrapper);
 
-    tooltipWrapper.appendChild(icon);
-    tooltipWrapper.appendChild(tooltip);
+      
+    }
 
-    iconContainer.appendChild(tooltipWrapper);
-  }
+    //GigaChad
+    if(miembro.rol == 'GigaChad'){
+      const icon = document.createElement('img');
+      icon.src = '../img/giga_icon.png';
+      icon.className = 'card-icon';
+      
+      const tooltipWrapper = document.createElement('span');
+      tooltipWrapper.className = 'tooltip';
 
-  //Lider
-  if(miembro.rol == 'Lider'){
-    const icon = document.createElement('img');
-    icon.src = '../img/lider_icon.png';
-    icon.className = 'card-icon';
-    
-    const tooltipWrapper = document.createElement('span');
-    tooltipWrapper.className = 'tooltip';
+      const tooltip = document.createElement('span');
+      tooltip.className = 'tooltiptext';
+      tooltip.textContent = 'GigaChad';
 
-    const tooltip = document.createElement('span');
-    tooltip.className = 'tooltiptext';
-    tooltip.textContent = 'Lider';
+      tooltipWrapper.appendChild(icon);
+      tooltipWrapper.appendChild(tooltip);
 
-    tooltipWrapper.appendChild(icon);
-    tooltipWrapper.appendChild(tooltip);
+      iconContainer.appendChild(tooltipWrapper);
+    }
 
-    iconContainer.appendChild(tooltipWrapper);
-  }
+    //MaxiChad
+    if(miembro.rol == 'MaxiChad'){
+      const icon = document.createElement('img');
+      icon.src = '../img/maxi_icon.png';
+      icon.className = 'card-icon';
+      
+      const tooltipWrapper = document.createElement('span');
+      tooltipWrapper.className = 'tooltip';
 
-  //shunter del mes
-  if(miembro.medallas.includes('b11')){
-    const icon = document.createElement('img');
-    icon.src = '../img/shunter_medal.png';
-    icon.className = 'card-icon';
-    
-    const tooltipWrapper = document.createElement('span');
-    tooltipWrapper.className = 'tooltip';
+      const tooltip = document.createElement('span');
+      tooltip.className = 'tooltiptext';
+      tooltip.textContent = 'MaxiChad';
 
-    const tooltip = document.createElement('span');
-    tooltip.className = 'tooltiptext';
-    tooltip.textContent = 'Shunter del mes';
+      tooltipWrapper.appendChild(icon);
+      tooltipWrapper.appendChild(tooltip);
 
-    tooltipWrapper.appendChild(icon);
-    tooltipWrapper.appendChild(tooltip);
+      iconContainer.appendChild(tooltipWrapper);
+    }
 
-    iconContainer.appendChild(tooltipWrapper);
-  }
+    //Lider
+    if(miembro.rol == 'Lider'){
+      const icon = document.createElement('img');
+      icon.src = '../img/lider_icon.png';
+      icon.className = 'card-icon';
+      
+      const tooltipWrapper = document.createElement('span');
+      tooltipWrapper.className = 'tooltip';
 
-  //race winner
-  if(miembro.medallas.includes('b13')){
-    const icon = document.createElement('img');
-    icon.src = '../img/racewinner_medal.png';
-    icon.className = 'card-icon';
-    
-    const tooltipWrapper = document.createElement('span');
-    tooltipWrapper.className = 'tooltip';
+      const tooltip = document.createElement('span');
+      tooltip.className = 'tooltiptext';
+      tooltip.textContent = 'Lider';
 
-    const tooltip = document.createElement('span');
-    tooltip.className = 'tooltiptext';
-    tooltip.textContent = 'Shiny Race';
+      tooltipWrapper.appendChild(icon);
+      tooltipWrapper.appendChild(tooltip);
 
-    tooltipWrapper.appendChild(icon);
-    tooltipWrapper.appendChild(tooltip);
+      iconContainer.appendChild(tooltipWrapper);
+    }
 
-    iconContainer.appendChild(tooltipWrapper);
-  }
+    //shunter del mes
+    if(miembro.medallas.includes('b11')){
+      const icon = document.createElement('img');
+      icon.src = '../img/shunter_medal.png';
+      icon.className = 'card-icon';
+      
+      const tooltipWrapper = document.createElement('span');
+      tooltipWrapper.className = 'tooltip';
 
-  //Golden dex
-  if(miembro.name == topData.trainer){
-    const icon = document.createElement('img');
-    icon.src = '../img/goldendex_medal.png';
-    icon.className = 'card-icon';
-    
-    const tooltipWrapper = document.createElement('span');
-    tooltipWrapper.className = 'tooltip';
+      const tooltip = document.createElement('span');
+      tooltip.className = 'tooltiptext';
+      tooltip.textContent = 'Shunter del mes';
 
-    const tooltip = document.createElement('span');
-    tooltip.className = 'tooltiptext';
-    tooltip.textContent = 'Maestro Apóstol';
+      tooltipWrapper.appendChild(icon);
+      tooltipWrapper.appendChild(tooltip);
 
-    tooltipWrapper.appendChild(icon);
-    tooltipWrapper.appendChild(tooltip);
+      iconContainer.appendChild(tooltipWrapper);
+    }
 
-    iconContainer.appendChild(tooltipWrapper);
-  }
+    //race winner
+    if(miembro.medallas.includes('b13')){
+      const icon = document.createElement('img');
+      icon.src = '../img/racewinner_medal.png';
+      icon.className = 'card-icon';
+      
+      const tooltipWrapper = document.createElement('span');
+      tooltipWrapper.className = 'tooltip';
 
-  //Supremo Pecador
-  if(miembro.name == 'AdrixJK'){
-    const icon = document.createElement('img');
-    icon.src = '../img/supremopecador_medal.png';
-    icon.className = 'card-icon';
-    
-    const tooltipWrapper = document.createElement('span');
-    tooltipWrapper.className = 'tooltip';
+      const tooltip = document.createElement('span');
+      tooltip.className = 'tooltiptext';
+      tooltip.textContent = 'Shiny Race';
 
-    const tooltip = document.createElement('span');
-    tooltip.className = 'tooltiptext';
-    tooltip.textContent = 'Supremo Pecador';
+      tooltipWrapper.appendChild(icon);
+      tooltipWrapper.appendChild(tooltip);
 
-    tooltipWrapper.appendChild(icon);
-    tooltipWrapper.appendChild(tooltip);
+      iconContainer.appendChild(tooltipWrapper);
+    }
 
-    iconContainer.appendChild(tooltipWrapper);
-  }
+    //Golden dex
+    if(miembro.name == topData.trainer){
+      const icon = document.createElement('img');
+      icon.src = '../img/goldendex_medal.png';
+      icon.className = 'card-icon';
+      
+      const tooltipWrapper = document.createElement('span');
+      tooltipWrapper.className = 'tooltip';
+
+      const tooltip = document.createElement('span');
+      tooltip.className = 'tooltiptext';
+      tooltip.textContent = 'Maestro Apóstol';
+
+      tooltipWrapper.appendChild(icon);
+      tooltipWrapper.appendChild(tooltip);
+
+      iconContainer.appendChild(tooltipWrapper);
+    }
+
+    //Supremo Pecador
+    if(miembro.name == 'AdrixJK'){
+      const icon = document.createElement('img');
+      icon.src = '../img/supremopecador_medal.png';
+      icon.className = 'card-icon';
+      
+      const tooltipWrapper = document.createElement('span');
+      tooltipWrapper.className = 'tooltip';
+
+      const tooltip = document.createElement('span');
+      tooltip.className = 'tooltiptext';
+      tooltip.textContent = 'Supremo Pecador';
+
+      tooltipWrapper.appendChild(icon);
+      tooltipWrapper.appendChild(tooltip);
+
+      iconContainer.appendChild(tooltipWrapper);
+    }
 
 
-  card.onclick = () => showInfo(key);
+    card.onclick = () => showInfo(key);
 
 
-  const img_wrap = document.createElement('div');
-  img_wrap.className = "card-img-wrapper";
-  card.appendChild(img_wrap);
+    const img_wrap = document.createElement('div');
+    img_wrap.className = "card-img-wrapper";
+    card.appendChild(img_wrap);
 
-  const img = document.createElement('img');
-  img.src = `../Members_sprites/${key}.png`;
-  img.onerror = function () {
-    this.src = `../Members_sprites/Placeholder.png`;
-  }
-  img.className = 'card-img';
-  img_wrap.appendChild(img);
+    const img = document.createElement('img');
+    img.src = `https://raw.githubusercontent.com/TeamMediChad/MedShowcaseDB/refs/heads/main/Members_sprites/${key}.png`;
+    img.onerror = function () {
+      this.src = `../Members_sprites/Placeholder.png`;
+    }
+    img.className = 'card-img';
+    img_wrap.appendChild(img);
 
-  const title = document.createElement('h2');
-  title.className = 'card-title';
-  title.textContent = miembro.name;
-  card.appendChild(title);
+    const title = document.createElement('h2');
+    title.className = 'card-title';
+    title.textContent = miembro.name;
+    card.appendChild(title);
 
-  // Crear rol
-  const role = document.createElement('p');
-  role.className = 'card-role';
-  role.textContent = miembro.rol;
-  card.appendChild(role);
+    // Crear rol
+    const role = document.createElement('p');
+    role.className = 'card-role';
+    role.textContent = miembro.rol;
+    card.appendChild(role);
 
-  member_container.appendChild(card);
+    member_container.appendChild(card);
 
-});
+  });
+}
+
--- scripts/shinydex.js
+++ scripts/shinydex.js
@@ -1,112 +1,111 @@
-function getSpriteURL(name) {
-    return `https://img.pokemondb.net/sprites/black-white/anim/shiny/${name}.gif`;
-}
+function renderShinydex(){
+    const topData = getTop(capturedBy);
+    const mainContent = document.getElementById("main-content2");
+
+    // Maestro apóstol
+    const topContainer = document.createElement("div");
+    topContainer.className = "top-capturer";
+
+    const topImg = document.createElement("img");
+    topImg.src = `https://raw.githubusercontent.com/TeamMediChad/MedShowcaseDB/refs/heads/main/Members_sprites/${topData.trainer}.png`;
+    topImg.className = "user-img";
+    topImg.onerror = () => {
+        topImg.remove();
+    };
+    const topText = document.createElement("div");
+    topText.textContent = `🎉 El que mas ha registrado en la Shinydex es ${topData.trainer} con ${topData.count} Pokemon.`;
+    topText.className = 'small-text-top';
+
+    const img_wrap = document.createElement('div');
+    img_wrap.className = "user-img-wrapper";
+    img_wrap.appendChild(topImg);
+    topContainer.appendChild(img_wrap);
+    topContainer.appendChild(topText);
+    mainContent.appendChild(topContainer);
+
+    let globalIndex = 0;
+
+    document.getElementById('Shinydex-header').textContent = `MeD Shinydex (${Object.keys(capturedBy).length + 1}/604)`;
 
 
-const mainContent = document.getElementById("main-content2");
+    //shinydex cards
+    for (const [genName, pokemonList] of Object.entries(generations)) {
+        const title = document.createElement("div");
+        title.className = "generation-title";
+        title.textContent = genName;
+        mainContent.appendChild(title);
 
-// Maestro apóstol
-const topContainer = document.createElement("div");
-topContainer.className = "top-capturer";
+        const grid = document.createElement("div");
+        grid.className = "grid-container";
 
-const topImg = document.createElement("img");
-topImg.src = `../Members_sprites/${topData.trainer}.png`;
-topImg.className = "user-img";
-topImg.onerror = () => {
-    topImg.remove();
-};
-const topText = document.createElement("div");
-topText.textContent = `🎉 El que mas ha registrado en la Shinydex es ${topData.trainer} con ${topData.count} Pokemon.`;
-topText.className = 'small-text-top';
+        pokemonList.forEach((name) => {
+            const container = document.createElement("div");
+            container.className = "shinydex-card";
 
-const img_wrap = document.createElement('div');
-img_wrap.className = "user-img-wrapper";
-img_wrap.appendChild(topImg);
-topContainer.appendChild(img_wrap);
-topContainer.appendChild(topText);
-mainContent.appendChild(topContainer);
+        if (Object.keys(capturedBy).includes(name)) {
+            container.classList.add("captured");
+            container.style.pointerEvents = "auto";
+            
+            const registrador = capturedBy[name] || "Desconocido";
+            
+            const tooltip = document.createElement("div");
+            tooltip.className = "shinydex-tooltip";
 
-let globalIndex = 0;
+            const img_wrap = document.createElement('div');
+            img_wrap.className = "user-img-wrapper";
 
-document.getElementById('Shinydex-header').textContent = `MeD Shinydex (${Object.keys(capturedBy).length + 1}/604)`;
+            exist = true;
+            const userImg = document.createElement("img");
+            userImg.src = `https://raw.githubusercontent.com/TeamMediChad/MedShowcaseDB/refs/heads/main/Members_sprites/${registrador}.png`;
+            userImg.alt = registrador;
+            userImg.onerror = () => {
+                exist = false;
+                userImg.innerHTML = '';
+                userImg.remove();
+            };
+            if(!exist){
+                img_wrap.innerHTML = '';
+                img_wrap.remove();
+            } else {
+                userImg.className = "user-img";
+                img_wrap.appendChild(userImg);
+                tooltip.appendChild(img_wrap);
+            }
 
+            const text = document.createElement("div");
+            text.textContent = `Registrado por: ${registrador}`;
+            tooltip.appendChild(text);
 
-//shinydex cards
-for (const [genName, pokemonList] of Object.entries(generations)) {
-    const title = document.createElement("div");
-    title.className = "generation-title";
-    title.textContent = genName;
-    mainContent.appendChild(title);
-
-    const grid = document.createElement("div");
-    grid.className = "grid-container";
-
-    pokemonList.forEach((name) => {
-        const container = document.createElement("div");
-        container.className = "shinydex-card";
-
-    if (Object.keys(capturedBy).includes(name)) {
-        container.classList.add("captured");
-        container.style.pointerEvents = "auto";
-        
-        const registrador = capturedBy[name] || "Desconocido";
-        
-        const tooltip = document.createElement("div");
-        tooltip.className = "shinydex-tooltip";
-
-        const img_wrap = document.createElement('div');
-        img_wrap.className = "user-img-wrapper";
-
-        exist = true;
-        const userImg = document.createElement("img");
-        userImg.src = `../Members_sprites/${registrador}.png`;
-        userImg.alt = registrador;
-        userImg.onerror = () => {
-            exist = false;
-            userImg.innerHTML = '';
-            userImg.remove();
-        };
-        if(!exist){
-            img_wrap.innerHTML = '';
-            img_wrap.remove();
-        } else {
-            userImg.className = "user-img";
-            img_wrap.appendChild(userImg);
-            tooltip.appendChild(img_wrap);
+            container.appendChild(tooltip);
         }
 
-        const text = document.createElement("div");
-        text.textContent = `Registrado por: ${registrador}`;
-        tooltip.appendChild(text);
+        const imgWrapper = document.createElement("div");
+        imgWrapper.className = "shinydex-wraper";
 
-        container.appendChild(tooltip);
+        const img = document.createElement("img");
+        img.src = getSpriteURL(name);
+        img.className = "shinydex-img";
+        img.alt = `Shiny ${name}`;
+
+        img.onerror = () => {
+            img.src = 'https://img.pokemondb.net/sprites/black-white/anim/shiny/missingno.gif';
+            img.alt = 'Imagen no disponible';
+        };
+
+        imgWrapper.appendChild(img);
+        
+
+        const label = document.createElement("div");
+        label.className = "shinydex-name";
+        label.textContent = name;
+
+        container.appendChild(imgWrapper);
+        container.appendChild(label);
+        grid.appendChild(container);
+        globalIndex++;
+        });
+
+        mainContent.appendChild(grid);
     }
 
-    const imgWrapper = document.createElement("div");
-    imgWrapper.className = "shinydex-wraper";
-
-    const img = document.createElement("img");
-    img.src = getSpriteURL(name);
-    img.className = "shinydex-img";
-    img.alt = `Shiny ${name}`;
-
-    img.onerror = () => {
-        img.src = 'https://img.pokemondb.net/sprites/black-white/anim/shiny/missingno.gif';
-        img.alt = 'Imagen no disponible';
-    };
-
-    imgWrapper.appendChild(img);
-    
-
-    const label = document.createElement("div");
-    label.className = "shinydex-name";
-    label.textContent = name;
-
-    container.appendChild(imgWrapper);
-    container.appendChild(label);
-    grid.appendChild(container);
-    globalIndex++;
-    });
-
-    mainContent.appendChild(grid);
 }
